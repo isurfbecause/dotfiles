a64cc6614d17b7feb0d54268d60b365a30927248
Winston Yee <winsyee@gmail.com>
2020-05-28 22:36:10 -0700

    polybar theme working

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Launch polybar
+#polybar main -c $HOME/.config/polybar/config.ini &
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload default &
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload main &
 done
 
-echo "Bars launched..."

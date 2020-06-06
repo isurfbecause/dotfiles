e64b40269df78d32ff583e2f7df14cb0f0ca88ff
Winston Yee <winsyee@gmail.com>
2020-06-05 23:45:46 -0700

    i3 and polybar config updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-
-BAR1=main
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,13 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-#polybar main -c $HOME/.config/polybar/config.ini &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload main &
+done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload "$BAR1" &
-  done
-else
-  polybar --reload "$BAR1" &
-fi
+echo "Bars launched..."
